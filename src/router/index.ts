7fdb5f8280d6c9d79b545b40de449b989c74bac9
MuriloJamoski <mugames2002@gmail.com>
2025-11-24 11:33:29 -0300

    feat: NAV feita

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import TvViews from '@/views/TvViews.vue'
 import TVDetailsView from '@/views/TVDetailsView.vue'
 import MovieDetailsView from '@/views/MovieDetailsView.vue'
 import Register from '@/components/FormsComponent.vue'
-
+import CantinhoView from '@/views/CantinhoView.vue' // novo
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
@@ -14,11 +14,12 @@ const routes = [
   { path: '/movie/:movieId', name: 'MovieDetails', component: MovieDetailsView, props: true },
   { path: '/tv/:tvId', name: 'TvDetails', component: TVDetailsView, props: true },
   { path: '/cadastro', name: 'Register', component: Register },
+  { path: '/cantinho', name: 'Cantinho', component: CantinhoView } // rota adicionada
 ];
-  
-  const router = createRouter({
-    history: createWebHistory(),
-    routes,
-  });
 
-  export default router;
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
